1bbbafd06a9d3467ce8d4a0e1e713f2ce6ef6aa4
Jason Gilmore <wj@wjgilmore.com>
2015-02-10 14:03:57 -0500

    Wrote /about view text

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,21 @@
+<h1>About TODOParrot</h1>
+
+<p>
+TODOParrot is the companion project to the book, <a href="http://easylaravelbook.com">Easy Laravel 5</a>, written by the bestselling author W. Jason Gilmore.
+</p>
+
+<p>
+TODOParrot is built atop Laravel 5 with the intent of giving fellow coders the opportunity to have a first-hand look at the many new features found in this major Laravel release. Additionally, TODOParrot demonstrates numerous Laravel features of interest to beginners, including:
+</p>
+
+<ul>
+<li>Blade templating</li>
+<li>Model relations</li>
+<li>Database queries</li>
+<li>Forms integration</li>
+<li>User authentication</li>
+</ul>
+
+<p>
+TODOParrot is released under the MIT license. You can download TODOParrot on GitHub. Found a bug or a better way to do something? File an issue or submit a pull request!
+</p>
