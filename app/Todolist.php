07f60415f27bad354ce0658349c94c94b8e2140e
Jason Gilmore <wj@wjgilmore.com>
2015-02-10 23:07:22 -0500

    Added hasMany task relation to Todolist model and added remainingTasks method

--- app/Todolist.php
+++ app/Todolist.php
@@ -15,5 +15,27 @@ class Todolist extends Model {
 	  return $this->belongsTo('Todoparrot\User');
 	}
 
+	/**
+	* Each list can be associated with one or more tasks.
+	*
+	*/
+	public function tasks()
+	{
+	return $this->hasMany('Todoparrot\Task');
+	}
+
+	/**
+	* Calculate the number of incomplete tasks
+	*
+	*/
+	public function remainingTasks()
+	{
+
+	$completed = $this->tasks()->where('done', '=', 1)->count();
+	$total = $this->tasks()->count();
+
+	return $total - $completed;
+
+	}
 
 }
