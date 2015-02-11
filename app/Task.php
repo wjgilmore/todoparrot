18ca4f33c61e44d371a47823cb8feae95095d799
Jason Gilmore <wj@wjgilmore.com>
2015-02-11 09:31:58 -0500

    Syntax cleanup

--- app/Task.php
+++ app/Task.php
@@ -4,8 +4,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model {
 
-	//
-
+	/**
+	 * Every task belongs to a Todolist
+	 * 
+	 */
 	public function Todolist()
 	{
 	  return $this->belongsTo('Todoparrot\Todolist');
--- app/Todolist.php
+++ app/Todolist.php
@@ -4,38 +4,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todolist extends Model {
 
-	protected $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description'];
 
-	/**
-	* Each list is owned by a registered user.
-	*
-	*/
-	public function user()
-	{
-	  return $this->belongsTo('Todoparrot\User');
-	}
+    /**
+    * Each list is owned by a registered user.
+    *
+    */
+    public function user()
+    {
+      return $this->belongsTo('Todoparrot\User');
+    }
 
-	/**
-	* Each list can be associated with one or more tasks.
-	*
-	*/
-	public function tasks()
-	{
-	return $this->hasMany('Todoparrot\Task');
-	}
+    /**
+    * Each list can be associated with one or more tasks.
+    *
+    */
+    public function tasks()
+    {
+    return $this->hasMany('Todoparrot\Task');
+    }
 
-	/**
-	* Calculate the number of incomplete tasks
-	*
-	*/
-	public function remainingTasks()
-	{
+    /**
+    * Calculate the number of incomplete tasks
+    *
+    */
+    public function remainingTasks()
+    {
 
-	$completed = $this->tasks()->where('done', '=', 1)->count();
-	$total = $this->tasks()->count();
+        $completed = $this->tasks()->where('done', '=', 1)->count();
+        $total = $this->tasks()->count();
 
-	return $total - $completed;
+        return $total - $completed;
 
-	}
+    }
 
 }
