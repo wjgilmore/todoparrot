6a3d34d596a9b75e0b6a5be6949266623d585124
Jason Gilmore <wj@wjgilmore.com>
2015-02-11 22:33:24 -0500

    Added setup and teardown methods to TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,32 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    public function setUp()
+    {
+      parent::setUp();
+      Artisan::call('migrate');
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+    }
 
-		return $app;
-	}
+    public function tearDown()
+    {
+      parent::setUp();
+      Artisan::call('migrate:reset');
+
+    }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
 
 }
