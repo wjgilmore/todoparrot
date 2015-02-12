973c8ffc08caa0e38bcdd91148144ec2ca429669
Jason Gilmore <wj@wjgilmore.com>
2015-02-11 22:33:06 -0500

    Added example validator to Todolist model

--- app/Todolist.php
+++ app/Todolist.php
@@ -6,6 +6,21 @@ class Todolist extends Model {
 
     protected $fillable = ['name', 'description'];
 
+    private $rules = [
+        'name' => 'required',
+        'description' => 'required'
+    ];
+
+    public function validate() 
+    {
+
+        $v = \Validator::make($this->attributes, $this->rules);
+        if ($v->passes()) return true;
+        $this->errors = $v->messages();
+        return false;
+
+    }
+
     /**
     * Each list is owned by a registered user.
     *
