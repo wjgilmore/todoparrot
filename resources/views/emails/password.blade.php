603d0303820460cfa5ec34fb6b542b5788479704
Jason Gilmore <wj@wjgilmore.com>
2015-02-12 10:43:30 -0500

    Updated the password recovery email

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,17 @@
+Greetings!
+
+You seem to have lost your password! Let's recover it so you can
+get back to managing TODO lists.
+
 Click here to reset your password: {{ url('password/reset/'.$token) }}
+
+Questions? Contact the TODOParrot team at todoparrot@wjgilmore.com!
+
+Want to buy a great Laravel 5 book? Check out Easy Laravel 5 at
+http://easylaravel.com!
+
+Productively Yours,
+The TODOParrot Team
+
+PS. DO NOT RESPOND TO THIS EMAIL. If you have a book question,
+email Jason (the author) at wj@wjgilmore.com.
