27e1d94d7e79c09fe4635f88814d21a224317c75
Jason Gilmore <wj@wjgilmore.com>
2015-02-12 11:01:11 -0500

    HTML formatted the password recovery e-mail. Duh.

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,17 +1,28 @@
+<p>
 Greetings!
+</p>
 
-You seem to have lost your password! Let's recover it so you can
-get back to managing TODO lists.
+<p>
+You seem to have lost your password! Let's recover it so you can get back to managing TODO lists.
+</p>
 
+<p>
 Click here to reset your password: {{ url('password/reset/'.$token) }}
+</p>
 
+<p>
 Questions? Contact the TODOParrot team at todoparrot@wjgilmore.com!
+</p>
 
-Want to buy a great Laravel 5 book? Check out Easy Laravel 5 at
-http://easylaravel.com!
+<p>
+Want to buy a great Laravel 5 book? Check out Easy Laravel 5 at http://easylaravel.com!
+</p>
 
-Productively Yours,
+<p>
+Productively Yours,<br />
 The TODOParrot Team
+</p>
 
-PS. DO NOT RESPOND TO THIS EMAIL. If you have a book question,
-email Jason (the author) at wj@wjgilmore.com.
+<p>
+PS. DO NOT RESPOND TO THIS EMAIL. If you have a book question, email Jason (the author) at wj@wjgilmore.com.
+</p>
