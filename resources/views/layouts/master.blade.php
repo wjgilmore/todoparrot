7113161364b7c9a1042f9542587778d7bc6e46e7
rongzedong <rongzedong@users.noreply.github.com>
2015-07-21 13:11:08 +0800

    Update master.blade.php
    apple-mobile-web-app-capable mode support.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome to TODOParrot</title>
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
