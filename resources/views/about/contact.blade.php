e1f4af6b9b7cf50201f3bf8bde67271bbf443ba9
Jason Gilmore <wj@wjgilmore.com>
2016-03-30 20:36:33 -0400

    Fixes #8. Thank you @unclepickles

--- resources/views/about/contact.blade.php
+++ resources/views/about/contact.blade.php
@@ -10,35 +10,32 @@
     @endforeach
 </ul>
 
-{!! Form::open(array('route' => 'contact_store', 'class' => 'form')) !!}
+{!! Form::open(['route' => 'contact_store', 'class' => 'form']) !!}
 
 <div class="form-group">
     {!! Form::label('Your Name') !!}
     {!! Form::text('name', null, 
-        array('required', 
-              'class'=>'form-control', 
-              'placeholder'=>'Your name')) !!}
+        ['class'=>'form-control', 
+              'placeholder'=>'Your name']) !!}
 </div>
 
 <div class="form-group">
     {!! Form::label('Your E-mail Address') !!}
     {!! Form::text('email', null, 
-        array('required', 
-              'class'=>'form-control', 
-              'placeholder'=>'Your e-mail address')) !!}
+        ['class'=>'form-control', 
+              'placeholder'=>'Your e-mail address']) !!}
 </div>
 
 <div class="form-group">
     {!! Form::label('Your Message') !!}
     {!! Form::textarea('message', null, 
-        array('required', 
-              'class'=>'form-control', 
-              'placeholder'=>'Your message')) !!}
+        ['class'=>'form-control', 
+              'placeholder'=>'Your message']) !!}
 </div>
 
 <div class="form-group">
     {!! Form::submit('Contact Us!', 
-      array('class'=>'btn btn-primary')) !!}
+      ['class'=>'btn btn-primary']) !!}
 </div>
 {!! Form::close() !!}
 
